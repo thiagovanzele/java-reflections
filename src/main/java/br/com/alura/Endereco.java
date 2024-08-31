f81940641a1cc308d1a12546a27337346cc75652
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-31 14:29:27 -0300

    feat: Should not Transform Teste

---
+++ src/main/java/br/com/alura/Endereco.java
@@ -0,0 +1,12 @@
+package br.com.alura;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Endereco {
+
+    private String rua;
+    private Integer numero;
+}
--- src/test/java/refl/TransformatorTest.java
+++ src/test/java/refl/TransformatorTest.java
@@ -1,5 +1,6 @@
 package refl;
 
+import br.com.alura.Endereco;
 import br.com.alura.PessoaDTO;
 import br.com.alura.transformation.Transformator;
 import br.com.alura.Pessoa;
@@ -12,6 +13,7 @@ import java.lang.reflect.InvocationTargetException;
 public class TransformatorTest {
 
     Pessoa pessoa = new Pessoa(1L, "João", "12345");
+    Endereco endereco = new Endereco("Rua 10", 20);
 
     @Test
     public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
@@ -21,5 +23,13 @@ public class TransformatorTest {
         Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO);
         Assertions.assertEquals(pessoa.getNome(), pessoaDTO.getNome());
         Assertions.assertEquals(pessoa.getCpf(), pessoaDTO.getCpf());
+    }
+
+    @Test
+    public void shouldNotTransform() {
+        Assertions.assertThrows(ClassNotFoundException.class, () -> {
+        Transformator transformator = new Transformator();
+        transformator.transform(endereco);
+        });
     }
 }
